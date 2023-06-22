f53ffec298edbab8e857007335da9988f2f43ede
michael_b <m.brueggemann7@web.de>
2023-06-22 20:08:08 +0200

    added documentation

--- model_code/load_sensitivity_analysis.py
+++ model_code/load_sensitivity_analysis.py
@@ -1,12 +1,16 @@
 import agentpy as ap
 import sensitivity_plots as splot
 
-# load results from "./sensitivity_data/<exp_name>_<exp_id>"
-results = ap.DataDict.load(exp_name='SA_Exp_Saltelli', exp_id="low_-2sd_up_+2sd_N1024_Iter_1_Agents_100", path='sensitivity_data', display=True)
+# run this script to perform the sensitivity analysis on already calculated model-outputs (e.g. with "run_sensitivity.py")
 
-# Calculate sobol indices on a given set of output metrics
-sob_results = results.calc_sobol(reporters=['mean_non_comp_prob', 'mean_nod'])
+if __name__ == "__main__":
+    # load results from "./sensitivity_data/<exp_name>_<exp_id>"
+    results = ap.DataDict.load(exp_name='SA_Exp_Saltelli', exp_id="low_-2sd_up_+2sd_N1024_Iter_1_Agents_100",
+                               path='sensitivity_data', display=True)
 
-# Plot sensitivity results as barchart.
-splot.plot_horizontal_stacked_barchart(sob_results)
-#splot.plot_vertical_barchart(sob_results)
+    # Calculate sobol indices on a given set of output metrics
+    sob_results = results.calc_sobol(reporters=['mean_non_comp_prob', 'mean_nod'])
+
+    # Plot sensitivity results as barchart.
+    splot.plot_horizontal_stacked_barchart(sob_results)
+    # splot.plot_vertical_barchart(sob_results)
